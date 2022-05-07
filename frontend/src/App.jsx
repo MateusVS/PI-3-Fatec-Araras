946acf0a7290742c91b605c000767f61d13765ac
mateusvs <mateusviniciussilva07@gmail.com>
2022-05-07 02:16:39 -0300

    Inicio Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import GlobalStyles from './styles/GlobalStyles';
+
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Menu />
+      <span>Hello World</span>
+    </div>
+  );
+}
+
+export default App;
