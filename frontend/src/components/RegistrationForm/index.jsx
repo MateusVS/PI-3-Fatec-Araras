dade21e8d1603250dd77dbc62c65e8cfaec4cfc9
MateusVS <mateusviniciussilva07@gmail.com>
2022-06-15 00:46:21 -0300

    Adição campos modal criação

--- frontend/src/components/RegistrationForm/index.jsx
+++ frontend/src/components/RegistrationForm/index.jsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import { Grid, TextField } from '@mui/material';
+import { Grid, TextField, Button } from '@mui/material';
 import { NotificationManager } from 'react-notifications';
 
 const formSchema = yup.object().shape({
@@ -20,11 +20,38 @@ function RegistrationForm() {
     <form onSubmit={() => handleSubmit()}>
       <Grid container spacing={4}>
         <Grid item lg={6}>
-          <TextField label="Full Name" variant='outlined' color='error' fullWidth {...register("FullName", {required: true})} />
+          <TextField label="Full Name" variant='outlined' color='error' fullWidth {...register("fullName", {required: true})} />
         </Grid>
         <Grid item lg={6}>
           <TextField label="Aliases" variant='outlined' color='error' fullWidth />
         </Grid>
+
+        <Grid item lg={3}>
+          <TextField label="Eye Color" variant='outlined' color='error' fullWidth />
+        </Grid>
+        <Grid item lg={3}>
+          <TextField label="Hair Color" variant='outlined' color='error' fullWidth />
+        </Grid>
+        <Grid item lg={6}>
+          <TextField label="Profession" variant='outlined' color='error' fullWidth />
+        </Grid>
+
+        <Grid item lg={4}>
+          <TextField label="First Apparition" variant='outlined' color='error' fullWidth />
+        </Grid>
+        <Grid item lg={4}>
+          <TextField label="Published By" variant='outlined' color='error' fullWidth />
+        </Grid>
+        <Grid item lg={4} container direction="column" alignItems="center" justifyContent="center">
+          <input accept="image/*" type="file" />
+        </Grid>
+
+      </Grid>
+
+      <Grid container direction="column" alignItems="center" justifyContent="center">
+        <Button variant="contained" type="submit" color="success" size="large" sx={{ width: '100px', marginTop: 5 }}>
+          Create
+        </Button>
       </Grid>
     </form>
   );
